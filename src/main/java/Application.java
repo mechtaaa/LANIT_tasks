f82477a599bc486d37e1089103cf5ca344a0b94e
mechtaaa <sanya_vybornov@bk.ru>
2022-06-10 11:12:02 +0400

    fix bug

--- src/main/java/Application.java
+++ src/main/java/Application.java
@@ -1,11 +1,11 @@
 import animals.Kotik;
 
 public class Application {
-    public static void main(String args[]) {
+    public static void main(String[] args ) {
         Kotik cat1 = new Kotik("Pushok", "meow", 3 , 10);
         Kotik cat2 = new Kotik();
         cat2.setName("Tim");
-        cat2.setVoice("myrr");
+        cat2.setVoice("myr");
         cat2.setSatiety(3);
         cat2.setWeight(8);
         cat1.liveAnotherDay();
@@ -13,11 +13,12 @@ public class Application {
             System.out.println(action);
         }
         System.out.println(cat2.getName()+" " + cat2.getWeight());
+
         compareVoice(cat1, cat2);
         System.out.println("Колличество котиков " + Kotik.getCount());
     }
-    private static boolean compareVoice(Kotik cat1, Kotik cat2) {
-        System.out.println(cat1.getVoice()==cat2.getVoice());
-        return true;
+
+    private static void compareVoice(Kotik cat1, Kotik cat2) {
+        System.out.println(cat1.getVoice().equals(cat2.getVoice()));
     }
 }
