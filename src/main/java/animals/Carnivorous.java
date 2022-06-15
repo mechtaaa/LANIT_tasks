70528da2a6bd25dda41b8c4a166cc92e97cb874f
mechtaaa <sanya_vybornov@bk.ru>
2022-06-15 19:49:33 +0400

    homework3

--- src/main/java/animals/Carnivorous.java
+++ src/main/java/animals/Carnivorous.java
@@ -17,7 +17,7 @@ public abstract class Carnivorous extends Animal {
                 throw new WrongFoodException("WrongFoodException");
             } else if (food instanceof Meat)
                 System.out.println(name + " ест мясо");
-            satiety++;
+            satiety = food.getEnergy();
         } catch (WrongFoodException e) {
             e.printStackTrace();
         }
