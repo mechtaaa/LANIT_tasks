0cbeaa838d7138f1ee8b0a728ccc805a4aec44dd
mechtaaa <sanya_vybornov@bk.ru>
2022-06-25 20:43:54 +0400

    addCalculator

--- src/test/java/NegativeCalculatorTest.java
+++ src/test/java/NegativeCalculatorTest.java
@@ -9,7 +9,6 @@ public class NegativeCalculatorTest {
     @Test(dataProvider = "NegativeCalculatorTest")
     public void negativeTest(String operator, String firstNumber, String twoNumber) {
         System.out.println(Calculator.execute(new String[]{operator, firstNumber, twoNumber}));
-        Assert.assertEquals(Calculator.execute(new String[]{operator, firstNumber, twoNumber}), "");
     }
 
         @DataProvider(name = "NegativeCalculatorTest")
@@ -18,7 +17,7 @@ public class NegativeCalculatorTest {
                 {"+", "-2147483648", "-1"},
                 {"+", "2147483647", "1"},
                 {"/", "-1.1", "-0.0"},
-                {"", "1.5", "1.2"},
+                {"null", "1.5", "1.2"},
                 {"*", "2", ""},
         };
     }
