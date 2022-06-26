d903e2e34c431e0e062655e85d91f14159be5bec
mechtaaa <sanya_vybornov@bk.ru>
2022-06-26 21:12:05 +0400

    addCalculator

--- src/test/java/PositiveCalculatorTest.java
+++ src/test/java/PositiveCalculatorTest.java
@@ -8,7 +8,7 @@ public class PositiveCalculatorTest{
     @Test(dataProvider = "PositiveCalculatorTest")
     public void positiveTest(String operator, String firstNumber, String twoNumber, double result) {
         System.out.println(Calculator.execute(new String[]{operator, firstNumber, twoNumber, String.valueOf(result)}));
-        //Assert.assertEquals(String.valueOf(result), Calculator.execute(new String[]{operator, firstNumber, twoNumber, String.valueOf(result)}));
+        // Assert.assertEquals(String.valueOf(result), Calculator.execute(new String[]{operator, firstNumber, twoNumber, String.valueOf(result)}));
     }
     @DataProvider(name = "PositiveCalculatorTest")
     public Object[][] positiveData() {
@@ -19,9 +19,8 @@ public class PositiveCalculatorTest{
                 {"*", "5", "5", 25},
                 {"-", "2.0", "1.1", 0.9},
                 {"/", "10", "2", 5},
-                {"-", String.valueOf(Integer.MAX_VALUE -0.1), "123", Integer.MAX_VALUE -123.01},
-                {"+", String.valueOf(Integer.MAX_VALUE -123), "122.99", Integer.MAX_VALUE -0.01},
-                {"+", String.valueOf(Integer.MIN_VALUE +1), "123", Integer.MIN_VALUE +124}
+                {"+", String.valueOf(Integer.MAX_VALUE -1), "1", Integer.MIN_VALUE +2147483647 },
+                {"+", String.valueOf(Integer.MIN_VALUE +1), "1", Integer.MAX_VALUE -2147483646 }
         };
     }
 }
