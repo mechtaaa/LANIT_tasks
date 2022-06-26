9e16a6967ede8b1481c5bacbbe19d9845af6bf2f
mechtaaa <sanya_vybornov@bk.ru>
2022-06-26 21:32:25 +0400

    addCalculator

--- src/main/java/model/Calculator.java
+++ src/main/java/model/Calculator.java
@@ -31,7 +31,7 @@ public class Calculator {
         if (result > Integer.MAX_VALUE || result < Integer.MIN_VALUE) {
                 throw new CalculatorException("Превышен порог значений");
             }
-        return String.format("%.3f", result);
+        return String.valueOf(result);
     }
 
     private static double calculate(String operator, double a, double b) {
--- src/test/java/PositiveCalculatorTest.java
+++ src/test/java/PositiveCalculatorTest.java
@@ -8,19 +8,18 @@ public class PositiveCalculatorTest{
     @Test(dataProvider = "PositiveCalculatorTest")
     public void positiveTest(String operator, String firstNumber, String twoNumber, double result) {
         System.out.println(Calculator.execute(new String[]{operator, firstNumber, twoNumber, String.valueOf(result)}));
-        // Assert.assertEquals(String.valueOf(result), Calculator.execute(new String[]{operator, firstNumber, twoNumber, String.valueOf(result)}));
+        //Assert.assertEquals(String.format("%.3f", result), Calculator.execute(new String[]{operator, firstNumber, twoNumber, String.valueOf(result)}));
     }
     @DataProvider(name = "PositiveCalculatorTest")
     public Object[][] positiveData() {
         return new Object[][]{
-                {"+", "2", "2", 5},
+                {"+", "2", "2", 4},
                 {"-", "5", "10", -5},
                 {"+", "-1", "1", 0},
                 {"*", "5", "5", 25},
                 {"-", "2.0", "1.1", 0.9},
                 {"/", "10", "2", 5},
-                {"+", String.valueOf(Integer.MAX_VALUE -1), "1", Integer.MIN_VALUE +2147483647 },
-                {"+", String.valueOf(Integer.MIN_VALUE +1), "1", Integer.MAX_VALUE -2147483646 }
+                {"-", String.valueOf(Integer.MAX_VALUE), String.valueOf(Integer.MAX_VALUE -6), 6 },
         };
     }
 }
